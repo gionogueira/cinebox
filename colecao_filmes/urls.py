f3a3cb66404b3c199c310bd05d21cbca6fb1c79d
20162014040015 <giovanna.rodrigues@academico.ifrn.edu.br>
2020-03-25 15:22:03 -0300

    Criação de login, registro e index

--- colecao_filmes/urls.py
+++ colecao_filmes/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from colecao_filmes.views import CadastroFilme 
-
+from colecao_filmes.views import register
 urlpatterns = [
-    path('', CadastroFilme.as_view(), name='cadastrofilme'),
+    path('', views.index, name='index'),
+    path('cadastrofilme/', CadastroFilme.as_view(), name='cadastrofilme'),
     path('colecaofilmes/', views.colecaofilmes, name='colecaofilmes'),
     path('editarfilme/<int:pk>/', views.editarfilme, name='editarfilme'),
     path('deletarfilme/<int:pk>/', views.deletarfilme, name='deletarfilme'),
+    path('registrar/', views.register.as_view(), name='registrar')
 ]
--- colecao_filmes/views.py
+++ colecao_filmes/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import redirect
 from .models import Filme
 from .forms import FilmeForm
 from django.views.generic import View
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+
+def index(request):
+    filme = Filme.objects.all()
+    return render(request, 'colecaofilmes/index.html', {'filme': filme})
 
 class CadastroFilme(View):
     def get(self, request):
@@ -41,4 +48,10 @@ def deletarfilme(request, pk):
     filme = Filme.objects.get(pk=pk)
     filme.delete()
     return redirect('colecaofilmes')
+
+class register(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
+
     
