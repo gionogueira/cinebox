e882bc2499ff0a339b6d8d687dbf70cf32e69c3c
20162014040015 <giovanna.rodrigues@academico.ifrn.edu.br>
2021-04-19 21:45:56 -0300

    Configuring the app

--- cinebox/settings.py
+++ cinebox/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://cinebox-web.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['cinebox-web.herokuapp.com', 'localhost']
 
 
 # Application definition
