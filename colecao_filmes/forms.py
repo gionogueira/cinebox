ecc47476b351568b1556384cdb14171861d494f4
20162014040015 <giovanna.rodrigues@academico.ifrn.edu.br>
2021-05-06 19:03:26 -0300

    Criação do editar perfil

--- colecao_filmes/forms.py
+++ colecao_filmes/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Filme, Genero
+from .models import Filme, Genero, Profile
+from django.contrib.auth.models import User
 
 class FilmeForm(forms.ModelForm):
     class Meta:
@@ -10,3 +11,15 @@ class GeneroForm(forms.ModelForm):
     class Meta:
         model = Genero
         fields = '__all__'
+
+class UserUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name']
+
+class ProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['imagem', 'cidade', 'estado', 'idade']
--- colecao_filmes/views.py
+++ colecao_filmes/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Filme, Genero, Profile
-from .forms import FilmeForm
+from .forms import FilmeForm, ProfileUpdateForm, UserUpdateForm
 from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-# from googletrans import Translator
-# import time
+from django.contrib import messages
 
 def index(request):
     return render(request, 'colecaofilmes/index.html', {})
@@ -67,9 +66,24 @@ class register(generic.CreateView):
 def profile(request):
     profile = Profile.objects.all()
     filme = Filme.objects.all()
+
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
     context = {
         'profile': profile,
-        'filme': filme
+        'filme': filme,
+        'u_form': u_form,
+        'p_form': p_form
     }
     return render(request, 'colecaofilmes/perfil.html', context)
 
